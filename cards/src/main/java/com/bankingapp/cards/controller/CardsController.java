73fa468d468ab8822ceeb118458a56c36990fa43
Pratik Gawand <gawand.p@northeastern.edu>
2023-05-27 23:25:16 -0400

    Cards MS

---
+++ cards/src/main/java/com/bankingapp/cards/controller/CardsController.java
@@ -0,0 +1,30 @@
+package com.bankingapp.cards.controller;
+
+import com.bankingapp.cards.model.Cards;
+import com.bankingapp.cards.model.Customer;
+import com.bankingapp.cards.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
+    @PostMapping("/myCards")
+    public List<Cards> getCardDetails(@RequestBody Customer customer) {
+        List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
+
+    }
+
+}
---
+++ cards/src/main/java/com/bankingapp/cards/repository/CardsRepository.java
@@ -0,0 +1,15 @@
+package com.bankingapp.cards.repository;
+
+import com.bankingapp.cards.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+
+    List<Cards> findByCustomerId(int customerId);
+
+}
