06199a05bd8eec4007fb702befa82d9864b81789
Pratik Gawand <gawand.p@northeastern.edu>
2023-05-27 23:20:08 -0400

    first commit

---
+++ accounts/src/main/java/com/bankingapp/accounts/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package com.bankingapp.accounts.repository;
+
+
+import com.bankingapp.accounts.model.Accounts;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+
+}
