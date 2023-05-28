e6e71b4bc0f0deeaae7f67f600e89ca60ab750e0
Pratik Gawand <gawand.p@northeastern.edu>
2023-05-27 23:22:56 -0400

    Loan MS

---
+++ loans/src/main/java/com/bankingapp/loans/repository/LoansRepository.java
@@ -0,0 +1,16 @@
+package com.bankingapp.loans.repository;
+
+import com.bankingapp.loans.model.Loans;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
